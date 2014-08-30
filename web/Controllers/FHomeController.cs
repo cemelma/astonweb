b20543d5c5b1d6511d4d93fad935ee37dfa6fcb3
unknown <cemelma.20@gmail.com>
2014-08-30 14:51:29 +0300

    mak

--- web/Controllers/FHomeController.cs
+++ web/Controllers/FHomeController.cs
@@ -33,7 +33,7 @@ namespace web.Controllers
         {
             HomePageWrapperModel model = new HomePageWrapperModel();
             model.photos = PhotoManager.GetListForFront(lang,0);
-            model.news = NewsManager.GetNewsListForFront(lang);
+            model.news = NewsManager.GetNewsListForFront(lang,0);
             model.servicegroups = ServiceManager.GetServiceList();
             model.references = ReferenceManager.GetReferenceListForFront(lang);
 
